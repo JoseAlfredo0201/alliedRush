44bd99b47904c90c3a913d5ebe018de2e98627d2
JuanAndresA016 <141958626+JuanAndresA016@users.noreply.github.com>
2025-06-03 13:29:47 -0600

    Nivel 3 y Game Over

---
+++ AlliedRush/Assets/Scripts/PlayerDeathHandler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeathHandler : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (player == null)
+        {
+
+            SceneManager.LoadScene(6);
+        }
+    }
+}
+
