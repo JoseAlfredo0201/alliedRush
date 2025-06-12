2308978fa00ee983ad6f13fbfce0b708f4304768
JuanAndresA016 <141958626+JuanAndresA016@users.noreply.github.com>
2025-06-12 00:26:00 -0600

    Nivel 3

---
+++ AlliedRush/Assets/Scripts/KeyDetector.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class KeyDetector : MonoBehaviour
+{
+    [SerializeField]
+    private string _colliderScript;
+
+    [SerializeField]
+    private UnityEvent _collisionEnter;
+
+    [SerializeField]
+    private UnityEvent _collisionExit;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent(_colliderScript))
+        {
+            _collisionEnter?.Invoke();
+        }
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent(_colliderScript))
+        {
+            _collisionExit?.Invoke();
+        }
+    }
+}
